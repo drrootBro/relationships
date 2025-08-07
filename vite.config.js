4fe4fa3d8afedef923996ec1f74fd9adc4d9abe0
drrootBro <firstvoteguy@gmail.com>
2025-08-06 21:55:02 -0400

    improve

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import polyfillNode from 'rollup-plugin-polyfill-node';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), polyfillNode()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      util: 'rollup-plugin-node-polyfills/polyfills/util',
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+      events: 'rollup-plugin-node-polyfills/polyfills/events',
+    },
+  },
 });
+
